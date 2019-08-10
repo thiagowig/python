e70d1a6986b0ca9fcef6acebf15505543b5d6b0a
Thiago Fonseca <dev.thiago@gmail.com>
2019-08-10 11:44:02 -0300

    Testing cloud firestore

--- poc-python-firestore/run-app.sh
+++ poc-python-firestore/run-app.sh
@@ -1,1 +1,1 @@
-export GOOGLE_APPLICATION_CREDENTIALS='/home/tfonseca/dev/keys/poc-python-firestore-5a4d77c48e6a.json' && pipenv run python3.7 main.py
+export GOOGLE_APPLICATION_CREDENTIALS='/home/thiago/dev/keys/poc-python-firestore-d26ec85589fd.json' && pipenv run python3 main.py
--- poc-python-firestore/src/user_gateway.py
+++ poc-python-firestore/src/user_gateway.py
@@ -15,23 +15,36 @@ class UserGateway():
 
     def list_one(client_id):
         users_ref = db.collection(collection_name)
-        doc = users_ref.document(client_id).get()
+        doc_ref = users_ref.document(client_id)
+        preferences = doc_ref.collection("order").stream()
 
-        return UserGateway.convert_object_to_dict(doc)
+        doc = doc_ref.get()
+        user_dict = UserGateway.convert_object_to_dict(doc)
+
+        user_dict["order"] = UserGateway.convert_array_to_dict(preferences)
 
 
+        return user_dict
+
+    """
     def create(client):
         doc_ref = db.collection(collection_name).document()
         doc_ref.set(client)
         doc = doc_ref.get()
 
-        doc_ref.collection("preferences").document().set(
+        doc_ref.collection(u"preferences").document().set(
             {
                 u'type': u'language',
                 u'name': u'java'
             }
         )
 
+        doc_ref.collection(u"preferences").document().set(
+            {
+                u'type': u'language',
+                u'name': u'python'
+            }
+        )
 
         response = {
             "message": "The client was created",
@@ -39,11 +52,103 @@ class UserGateway():
         }
 
         return response
+    """
 
+    """
+    def create(client):
+        batch = db.batch()
+
+        doc_ref = db.collection(collection_name).document()
+
+        batch.set(doc_ref, client)
+
+        doc = doc_ref.get()
+
+        pref_01 = doc_ref.collection(u"preferences").document()
+        batch.set(pref_01, {
+            u'type': u'language',
+            u'name': u'java'
+        })
+
+        raise Exception("JFF 02")
+
+        pref_02 = doc_ref.collection(u"preferences").document()
+        batch.set(pref_02, {
+            u'type': u'language',
+            u'name': u'python'        
+        })
+
+        batch.commit()
+
+        response = {
+            "message": "The client was created",
+            "id": doc.id
+        }
+
+        return response
+    """
+    
+    """
+    def create(client):
+        transaction = db.transaction()
+
+        @firestore.transactional
+        def update_in_transaction(transaction):
+            doc_ref = db.collection(collection_name).document()
+
+            transaction.set(doc_ref, client)
+
+            doc = doc_ref.get()
+
+            pref_01 = doc_ref.collection(u"preferences").document()
+            transaction.set(pref_01, {
+                u'type': u'language',
+                u'name': u'java'
+            })
+
+            #raise Exception("JFF 02")
+
+            pref_02 = doc_ref.collection(u"preferences").document()
+            transaction.set(pref_02, {
+                u'type': u'language',
+                u'name': u'python'        
+            })
+
+            return doc.id
+
+        id = update_in_transaction(transaction)
+
+        response = {
+            "message": "The client was created",
+            "id": id
+        }
+
+        return response
+    """
+
+
+    def create(client):
+        order_products = client["order"]
+        del client["order"]
+
+        client_ref = db.collection(collection_name).document()
+        order_ref = client_ref.collection("order")
+        
+        
+        client_ref.set(client)
+        for product in order_products:
+            order_ref.document().set(product)
+
+        response = {
+            "message": "The client was created",
+            "id": client_ref.get().id
+        }
+
+        return response
 
     def update(client_id, client):
         users_ref = db.collection(collection_name).document(client_id)
-        users_ref.update(client)
+        users_ref.set(client, merge=False)
 
         return {'message': 'The client was updated'}
 
@@ -65,10 +170,11 @@ class UserGateway():
 
 
     def find_by_age_greater_than(age):
-        users_ref = db.collection(collection_name)
+        collection_ref = db.collection_group(u"order")
         #query = users_ref.where(u'age', u'>=', 18).where(u'age', u'<=', 30)
         #query = users_ref.where(u'last_name', u'==', "Augusto")
         #query = users_ref.where(u'preferences', u'array_contains', "typescript")
+        query = collection_ref.where(u'product_id', u'==', 777)
         docs = query.stream()
 
         return UserGateway.convert_array_to_dict(docs)
